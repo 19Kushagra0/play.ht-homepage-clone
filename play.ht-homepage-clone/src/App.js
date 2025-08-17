8936b139faf914d4143d44a84b64f115b786401f
19Kushagra0 <19Kushagra0@gmail.com>
2025-08-17 22:56:29 +0530

    title created

--- play.ht-homepage-clone/src/App.js
+++ play.ht-homepage-clone/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./myComponents/1_Header/Header";
+import Title from "./myComponents/2_Title/Title";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Title />
     </div>
   );
 }
