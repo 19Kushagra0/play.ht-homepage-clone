0fe3d3ca55d3641f5c45bf73d68a4019244ead79
19kushagra0 <19Kushagra0@gmail.com>
2026-05-20 17:07:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./myComponents/1_Header/Header";
 import Title from "./myComponents/2_Title/Title";
 import Input from "./myComponents/3_Input/Input";
 import Users from "./myComponents/4_Users/Users";
-import Voice_generator from "./myComponents/5_Voice_generator/Voice_generator";
+import VoiceGenerator from "./myComponents/5_Voice_generator/Voice_generator";
 import Feature from "./myComponents/6_Feature/Feature";
-import Feature_2 from "./myComponents/7_Feature_2/Feature_2 ";
+import Feature2 from "./myComponents/7_Feature_2/Feature_2 ";
 import Leading from "./myComponents/8_Leading/Leading";
 import Voices from "./myComponents/9_Voices/Voices";
 import Synthesis from "./myComponents/10_Synthesis/Synthesis";
@@ -22,9 +21,9 @@ function App() {
       <Title />
       <Input />
       <Users />
-      <Voice_generator />
+      <VoiceGenerator />
       <Feature />
-      <Feature_2 />
+      <Feature2 />
       <Leading />
       <Voices />
       <Synthesis />
@@ -37,3 +36,4 @@ function App() {
 }
 
 export default App;
+
